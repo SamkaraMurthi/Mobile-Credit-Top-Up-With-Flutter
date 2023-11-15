169f3581a79f784902fd33bbbdb741c0df089b75
Ichi Pravda <ichipravda@IchiPravda.IchiPravda.com>
2023-11-15 18:24:18 +0100

    just some change on homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pulsa_login/main_page.dart' as colors;
 
 class home_page extends StatefulWidget {
   const home_page({super.key});
@@ -10,8 +11,24 @@ class home_page extends StatefulWidget {
 class _home_pageState extends State<home_page> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("HOME PAGE"),
-    );
+    return Container(
+        child: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.12,
+          decoration: BoxDecoration(
+            gradient: myColors.backgroundColor1,
+          ),
+        )
+      ],
+    ));
   }
+}
+
+class myColors {
+  static const backgroundColor1 = LinearGradient(
+    colors: [Colors.pink, Colors.purple],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
 }
